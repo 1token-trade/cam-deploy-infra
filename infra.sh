bbdfa4e75369204c746879752d44aa7e9130cfd5
Youze Tang <tyz@1token.trade>
2021-03-04 16:06:39 +0800

    Update infra.sh

--- infra.sh
+++ infra.sh
@@ -1,2 +1,6 @@
 sudo apt update -y
 sudo apt upgrade -y
+sudo apt install docker.io pass gnupg2 docker-compose tmux -y 
+sudo usermod -aG docker $USER 
+# change default shell to bash 
+sudo chsh -s $(which bash) $USER 
