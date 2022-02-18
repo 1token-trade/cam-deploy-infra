b7c330c6cc8fa36af3a5fefac6ce93234e20a4ec
Zhangkai Liu <liu.zhangkai@hotmail.com>
2022-02-18 16:49:32 +0800

    Update infra.sh

--- infra.sh
+++ infra.sh
@@ -1,7 +1,7 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove -y
-sudo apt install docker.io pass gnupg2 docker-compose tmux -y 
+sudo apt install docker.io pass gnupg2 docker-compose tmux python3 python3-pip -y 
 sudo usermod -aG docker $USER 
 # change default shell to bash 
 sudo chsh -s $(which bash) $USER 
