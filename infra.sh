6761fd7c04a4e3b503fdbd11f986af3fd952cc8b
Zhangkai Liu <liu.zhangkai@hotmail.com>
2024-03-29 13:30:21 +0800

    install and config chrony for AWS

--- infra.sh
+++ infra.sh
@@ -6,6 +6,15 @@ sudo usermod -aG docker $USER
 # change default shell to bash 
 sudo chsh -s $(which bash) $USER 
 
+# install and config chrony service for AWS
+if [ ! -f /etc/chrony/chrony.conf ]; then
+    echo 'install chrony'
+    sudo apt install chrony -y
+    sudo cp /etc/chrony/chrony.conf /etc/chrony/chrony.conf.bak
+    sudo sed -i '16a server 169.254.169.123 prefer iburst minpoll 4 maxpoll 4' /etc/chrony/chrony.conf
+    sudo /etc/init.d/chrony restart
+    chronyc tracking
+fi
 # change docker log size
 echo '{"log-opts":{"max-size":"500m","max-file":"5","compress":"true"}}' |sudo tee  /etc/docker/daemon.json
 echo 'mark docker service not auto-upgrade'
